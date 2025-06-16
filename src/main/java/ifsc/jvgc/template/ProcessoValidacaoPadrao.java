d4f337a1a14604f288f47f96bf629d0bb55951ce
jvictorgcruz <jvictorgcruz@gmail.com>
2025-06-16 19:42:32 -0300

    fix: add abstract extension syntax

--- src/main/java/ifsc/jvgc/template/ProcessoValidacaoPadrao.java
+++ src/main/java/ifsc/jvgc/template/ProcessoValidacaoPadrao.java
@@ -2,7 +2,7 @@ package ifsc.jvgc.template;
 
 import ifsc.jvgc.model.AtividadeRealizada;
 
-public class ProcessoValidacaoPadrao {
+public class ProcessoValidacaoPadrao extends ProcessoValidacaoAtividade {
     protected void verificarDocumento(AtividadeRealizada atividade) {
         System.out.println("Verificando documento: " + atividade.documento());
     }
