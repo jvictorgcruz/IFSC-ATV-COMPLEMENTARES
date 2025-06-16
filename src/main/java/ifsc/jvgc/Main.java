768e2f83b6c6f9ffada68f81f0b0f335d5d66bf0
jvictorgcruz <jvictorgcruz@gmail.com>
2025-06-16 19:55:59 -0300

    feat: add basic example

--- src/main/java/ifsc/jvgc/Main.java
+++ src/main/java/ifsc/jvgc/Main.java
@@ -1,7 +1,33 @@
 package ifsc.jvgc;
 
+import ifsc.jvgc.model.*;
+import ifsc.jvgc.strategy.*;
+import ifsc.jvgc.template.*;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
-        }
+        Aluno aluno = new Aluno(1, "Ana");
+        DocumentacaoComprobatoria doc = new DocumentacaoComprobatoria(1, "Certificado");
+        Modalidade modalidade = new Modalidade(1, "Ensino");
+        HorasPorAtividade horas = new HorasPorAtividade(1, "Conforme documento");
+
+        Requerimento requerimento = new Requerimento(1, aluno, LocalDate.of(2025, 4, 20),
+                null);
+        AtividadeComplementar atividadeComp = new AtividadeComplementar(1, "Curso de Extensão", doc, horas,
+                40, modalidade);
+        AtividadeRealizada atividadeRealizada = new AtividadeRealizada(1, requerimento, atividadeComp,
+                30, "certificado.pdf");
+
+        // Usando STRATEGY + TEMPLATE METHOD
+        EstrategiaValidacaoHoras estrategia = new ValidacaoComLimiteMaximo();
+        ProcessoValidacaoAtividade processo = new ProcessoValidacaoPadrao();
+        processo.validar(atividadeRealizada, estrategia);
+
+        // Usando STATE
+        requerimento.avaliar();
+        requerimento.avaliar();
+        requerimento.avaliar();
+    }
 }
